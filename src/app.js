c76e855fcfbcce9ac0c8c95fc23ff10ae544394e
[REDACTED] <54645548+StrongSpider@users.noreply.github.com>
2021-08-28 15:35:23 -0400

    HTML Up

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(express.static("public"))
 
 // define the first route
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+  res.sendfile("../public/main.html")
 })
 
 // start the server listening for requests
